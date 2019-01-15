ffaa3b53e32bff81f3f2cb120bc22b62053e335c
vlad <slavayus@gmail.com>
2019-01-15 13:23:48 +0300

    initial commit

---
+++ src/main/java/SampleClient.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.Socket;
+
+class SampleClient {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("0.0.0.0", 7007);
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+             DataInputStream in = new DataInputStream(socket.getInputStream())) {
+
+            System.out.println("Client connected to socket.");
+            System.out.println();
+            System.out.println("Client writing channel = out & reading channel = in initialized.");
+
+            while (!socket.isOutputShutdown()) {
+                if (br.ready()) {
+                    System.out.println("Client start writing in channel...");
+                    String clientCommand = br.readLine();
+
+                    out.writeUTF(clientCommand);
+                    out.flush();
+                    System.out.println("Client sent message " + clientCommand + " to server.");
+                    if (clientCommand.equalsIgnoreCase("quit")) {
+                        break;
+                    }
+
+                    System.out.println("Client sent message & start waiting for data from server...");
+                    if (in.available() > 0) {
+                        System.out.println("reading...");
+                        System.out.println(in.readUTF());
+                    }
+                }
+            }
+            System.out.println("Closing connections & channels on clentSide - DONE.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
